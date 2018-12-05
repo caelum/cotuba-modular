965809e6653be4a7bfe4fc514e85031d00350805
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-05 18:17:46 -0200

    passo 3

--- cotuba/cotuba-core/src/main/java/cotuba/domain/FormatoEbook.java
+++ cotuba/cotuba-core/src/main/java/cotuba/domain/FormatoEbook.java
@@ -1,22 +1,6 @@
 package cotuba.domain;
 
-import cotuba.epub.GeradorEPUB;
-import cotuba.pdf.GeradorPDF;
-import cotuba.plugin.GeradorEbook;
-
 public enum FormatoEbook {
 
-	PDF(new GeradorPDF()),
-	EPUB(new GeradorEPUB());
-
-	private GeradorEbook gerador;
-
-	private FormatoEbook(GeradorEbook gerador) {
-		this.gerador = gerador;
-	}
-
-	public GeradorEbook getGerador() {
-		return gerador;
-	}
-
+	PDF, EPUB;
 }
