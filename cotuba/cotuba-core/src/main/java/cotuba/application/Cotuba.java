e76a0902279dc3be518bf0dfcc468496544a8ea0
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-06 16:11:18 -0200

    passo 7

--- cotuba/cotuba-core/src/main/java/cotuba/application/Cotuba.java
+++ cotuba/cotuba-core/src/main/java/cotuba/application/Cotuba.java
@@ -12,14 +12,13 @@ import cotuba.plugin.GeradorEbook;
 
 public class Cotuba {
 
-	public void executa(ParametrosCotuba parametros, Consumer<String> acaoPosGeracao) {
+	public void executa(ParametrosCotuba parametros, Consumer<String> acaoPosGeracao, RepositorioDeMDs repositorioDeMDs) {
 
 		FormatoEbook formato = parametros.getFormato();
-		Path diretorioDosMD = parametros.getDiretorioDosMD();
 		Path arquivoDeSaida = parametros.getArquivoDeSaida();
 
 		RenderizadorMDParaHTML renderizador = RenderizadorMDParaHTML.cria();
-		List<Capitulo> capitulos = renderizador.renderiza(diretorioDosMD);
+		List<Capitulo> capitulos = renderizador.renderiza(repositorioDeMDs);
 
 		Ebook ebook = new Ebook(formato, arquivoDeSaida, capitulos);
 
