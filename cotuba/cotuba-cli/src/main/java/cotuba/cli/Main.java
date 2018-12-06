584aadf80bab9b76d39b4c6ede55d3098e9aff8c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-06 16:46:41 -0200

    passo 1

--- cotuba/cotuba-cli/src/main/java/cotuba/cli/Main.java
+++ cotuba/cotuba-cli/src/main/java/cotuba/cli/Main.java
@@ -4,10 +4,17 @@ import java.nio.file.Path;
 
 import cotuba.application.Cotuba;
 import cotuba.application.RepositorioDeMDs;
+import cotuba.domain.builder.CapituloBuilder;
+import cotuba.md.RenderizadorMDParaHTMLComCommonMark;
+import cotuba.tema.AplicadorTema;
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		CapituloBuilder builder = new CapituloBuilder();
+		RenderizadorMDParaHTMLComCommonMark md = new RenderizadorMDParaHTMLComCommonMark();
+		AplicadorTema tema = new AplicadorTema();
 
 		LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
 
