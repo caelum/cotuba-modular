26d958f2a7326bd55815109a58300c3883a2fbf8
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-06 17:02:06 -0200

    passo 2

--- cotuba/cotuba-core/src/main/java/module-info.java
+++ cotuba/cotuba-core/src/main/java/module-info.java
@@ -1,10 +1,7 @@
 module cotuba.core {
 	exports cotuba.application;
-	exports cotuba.domain.builder;
 	exports cotuba.plugin;
-	exports cotuba.tema;
 	exports cotuba.domain;
-	exports cotuba.md;
 
 	requires jsoup;
 	requires org.commonmark;
