fbcd82a876e5877c791a2b12cb4030c86444c960
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-06 15:35:52 -0200

    passo 2

---
+++ cotuba/cotuba-web/src/main/java/cotuba/web/controller/GeracaoDeLivrosController.java
@@ -0,0 +1,36 @@
+package cotuba.web.controller;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import cotuba.web.SpringFileUtils;
+
+@Controller
+public class GeracaoDeLivrosController {
+
+	@GetMapping("/livros/{id}/pdf")
+	public ResponseEntity<ByteArrayResource> geraPDF(@PathVariable("id") Long id, Model model) {
+
+		Path pdf = Paths.get("/home/alexandre/Desktop/book.pdf");
+
+		return SpringFileUtils.montaResponseOArquivo(pdf, "application/pdf");
+
+	}
+
+	@GetMapping("/livros/{id}/epub")
+	public ResponseEntity<ByteArrayResource> geraEPUB(@PathVariable("id") Long id, Model model) {
+
+		Path epub = Paths.get("/home/alexandre/Desktop/book.epub");
+
+		return SpringFileUtils.montaResponseOArquivo(epub, "application/epub+zip");
+
+	}
+
+}
