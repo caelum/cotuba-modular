0a076f17e405af0c337b150459589cbed1120f8e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-06 15:38:42 -0200

    passo 3

--- cotuba/cotuba-web/src/main/java/cotuba/web/application/GeracaoDeLivros.java
+++ cotuba/cotuba-web/src/main/java/cotuba/web/application/GeracaoDeLivros.java
@@ -1,6 +1,11 @@
 package cotuba.web.application;
 
+import java.nio.file.Path;
+
 import org.springframework.stereotype.Service;
+
+import cotuba.domain.FormatoEbook;
+import cotuba.web.domain.Livro;
 
 @Service
 public class GeracaoDeLivros {
@@ -9,5 +14,10 @@ public class GeracaoDeLivros {
 
 	public GeracaoDeLivros(CadastroDeLivros livros) {
 		this.livros = livros;
+	}
+
+	public Path geraLivro(Long id, FormatoEbook formato) { // inserido
+		Livro livro = livros.detalha(id); // inserido
+		// CHAMADA DO COTUBA AQUI..
 	}
 }
