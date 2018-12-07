c91b2840dc84387efde7598ac9d23eec0902fa5f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-07 17:20:11 -0200

    passo 3

---
+++ cotuba/cotuba-cli/src/main/java/cotuba/cli/SpringConfig.java
@@ -0,0 +1,10 @@
+package cotuba.cli;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cotuba")
+public class SpringConfig {
+
+}
