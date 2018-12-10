962c30adc9a3177cf1b5fca9f19cafd604a8894a
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-10 15:35:50 -0200

    passo 1

--- cotuba/cotuba-web/src/main/java/cotuba/web/Boot.java
+++ cotuba/cotuba-web/src/main/java/cotuba/web/Boot.java
@@ -2,7 +2,9 @@ package cotuba.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("cotuba")
 @SpringBootApplication
 public class Boot {
 	
