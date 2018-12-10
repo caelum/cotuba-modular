ee923ded34c9d049452c8026cf5e9a569ae8713c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-10 15:31:48 -0200

    passo 11

--- cotuba/cotuba-core/src/main/java/cotuba/application/Cotuba.java
+++ cotuba/cotuba-core/src/main/java/cotuba/application/Cotuba.java
@@ -2,7 +2,10 @@ package cotuba.application;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
+
+import org.springframework.stereotype.Component;
 
 import cotuba.domain.Capitulo;
 import cotuba.domain.Ebook;
@@ -10,23 +13,39 @@ import cotuba.domain.FormatoEbook;
 import cotuba.plugin.AoFinalizarGeracao;
 import cotuba.plugin.GeradorEbook;
 
+@Component
 public class Cotuba {
 
-	public void executa(ParametrosCotuba parametros, Consumer<String> acaoPosGeracao, RepositorioDeMDs repositorioDeMDs) {
+	private final RenderizadorMDParaHTML renderizador;
+	private final Map<FormatoEbook, GeradorEbook> geradores;
+	private final List<AoFinalizarGeracao> finalizacoes;
+
+	public Cotuba(RenderizadorMDParaHTML renderizador, Map<FormatoEbook, GeradorEbook> geradores,
+			List<AoFinalizarGeracao> finalizacoes) {
+		this.renderizador = renderizador;
+		this.geradores = geradores;
+		this.finalizacoes = finalizacoes;
+	}
+
+	public void executa(ParametrosCotuba parametros, Consumer<String> acaoPosGeracao,
+			RepositorioDeMDs repositorioDeMDs) {
 
 		FormatoEbook formato = parametros.getFormato();
 		Path arquivoDeSaida = parametros.getArquivoDeSaida();
 
-		RenderizadorMDParaHTML renderizador = RenderizadorMDParaHTML.cria();
 		List<Capitulo> capitulos = renderizador.renderiza(repositorioDeMDs);
 
 		Ebook ebook = new Ebook(formato, arquivoDeSaida, capitulos);
 
-		GeradorEbook gerador = GeradorEbook.cria(formato);
+		GeradorEbook gerador = geradores.get(formato);
+		if (gerador == null) {
+			throw new RuntimeException("Não há gerador de livro para o formato " + formato);
+		}
 		gerador.gera(ebook);
 
-		AoFinalizarGeracao.gerou(ebook, acaoPosGeracao);
-
+		for (AoFinalizarGeracao finalizacao : finalizacoes) {
+			finalizacao.aposGeracao(ebook, acaoPosGeracao);
+		}
 	}
 
 }
