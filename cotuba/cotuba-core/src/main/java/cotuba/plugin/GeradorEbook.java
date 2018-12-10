52cfe0ccb253e6464f9130d3644fb2903214e79c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-10 15:29:32 -0200

    passo 10

--- cotuba/cotuba-core/src/main/java/cotuba/plugin/GeradorEbook.java
+++ cotuba/cotuba-core/src/main/java/cotuba/plugin/GeradorEbook.java
@@ -1,7 +1,5 @@
 package cotuba.plugin;
 
-import java.util.ServiceLoader;
-
 import cotuba.domain.Ebook;
 import cotuba.domain.FormatoEbook;
 
@@ -10,14 +8,5 @@ public interface GeradorEbook {
 	void gera(Ebook ebook);
 
 	FormatoEbook formato();
-
-	public static GeradorEbook cria(FormatoEbook formato) {
-		for (GeradorEbook gerador : ServiceLoader.load(GeradorEbook.class)) {
-			if (gerador.formato().equals(formato)) {
-				return gerador;
-			}
-		}
-		throw new RuntimeException("Formato do ebook inválido: " + formato);
-	}
 
 }
