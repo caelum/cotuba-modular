29bdfc4fc1ab1b81d1bcc6c287b63c22bdc34c49
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-10 15:21:20 -0200

    passo 4

--- cotuba/cotuba-core/src/main/java/cotuba/tema/AplicadorTema.java
+++ cotuba/cotuba-core/src/main/java/cotuba/tema/AplicadorTema.java
@@ -4,17 +4,25 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
 
 import cotuba.plugin.Tema;
 
+@Component
 public class AplicadorTema {
+
+	private final List<Tema> temas;
+
+	public AplicadorTema(List<Tema> temas) {
+		this.temas = temas;
+	}
 
 	public String aplica(String html) {
 
 		Document document = Jsoup.parse(html);
 
-		List<String> listaDeTemas = Tema.listaDeTemas();
-		for (String css : listaDeTemas) {
+		for (Tema tema : temas) {
+			String css = tema.cssDoTema();
 			document.select("head").append("<style> " + css + " </style>");
 		}
 
