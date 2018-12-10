4c969eab8852744e6ed919b9ff8ed307723cb763
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-12-10 15:25:13 -0200

    passo 6

--- cotuba/cotuba-core/src/main/java/cotuba/application/RenderizadorMDParaHTML.java
+++ cotuba/cotuba-core/src/main/java/cotuba/application/RenderizadorMDParaHTML.java
@@ -3,14 +3,9 @@ package cotuba.application;
 import java.util.List;
 
 import cotuba.domain.Capitulo;
-import cotuba.md.RenderizadorMDParaHTMLComCommonMark;
 
 public interface RenderizadorMDParaHTML {
 
 	List<Capitulo> renderiza(RepositorioDeMDs repositorioDeMDs);
-
-	public static RenderizadorMDParaHTML cria() {
-		return new RenderizadorMDParaHTMLComCommonMark();
-	}
 
 }
